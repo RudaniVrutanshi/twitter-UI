e65e441c845d3643967483d78e12b61876fc6fd9
vrutanshi <sonanizalak7@gmail.com>
2024-07-21 17:08:27 +0530

    3- usefollow hook added

--- src/components/common/RightPanel.jsx
+++ src/components/common/RightPanel.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import {useQuery} from '@tanstack/react-query';
+import useFollow from "../../hooks/useFollow";
+
+import LoadingSpinner from "./LoadingSpinner";
 
 const RightPanel = () => {
 	
@@ -20,6 +23,8 @@ const RightPanel = () => {
 			}
 		},
 	});
+
+	const {follow,isPending} = useFollow();
 
 	if(suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
@@ -60,8 +65,13 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => 
-											e.preventDefault()}>Follow</button>
+										onClick={(e) => {
+											e.preventDefault();
+											follow(user._id);
+										}
+											}>
+												{isPending ? <LoadingSpinner size="sm"/> : "Follow"}
+											</button>
 								</div>
 							</Link>
 						))}
